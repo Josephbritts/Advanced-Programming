4d46b462991a303a99bda0a41f52ab17667af3ea
Josephbritts <josephbritts901@gmail.com>
2026-03-26 18:51:57 +0000

    last push i guess

---
+++ scr/main/java/comp1549/Security/capability/Operation.java
@@ -0,0 +1,4 @@
+package comp1549.task2.security.capability;
+
+public interface Operation {
+}
---
+++ scr/main/java/comp1549/Security/capability/ReadPermission.java
@@ -0,0 +1,4 @@
+package comp1549.task2.security.capability;
+
+public final class ReadPermission implements Operation {
+}
